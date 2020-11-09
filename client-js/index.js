b3902a0b58a4e1e0913a893baff03a209100e4b3
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2020-11-09 16:21:54 +0000

    feat: add files and scripts for accessible textarea maxlength

--- client-js/index.js
+++ client-js/index.js
@@ -4,6 +4,7 @@ var toolkit = require('hof-frontend-toolkit');
 var helpers = toolkit.helpers;
 var progressiveReveal = toolkit.progressiveReveal;
 var formFocus = toolkit.formFocus;
+var characterCount = toolkit.characterCount;
 
 var cookieSettings = require('./cookieSettings');
 
@@ -13,3 +14,4 @@ helpers.documentReady(progressiveReveal);
 helpers.documentReady(formFocus);
 helpers.documentReady(cookieSettings.initialiseCookieBanner);
 helpers.documentReady(cookieSettings.initialiseCookiePage);
+helpers.documentReady(characterCount);
